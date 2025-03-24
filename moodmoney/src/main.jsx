a24fe4486406d763a781a2e5fa93da2f61a35763
Joel Aloma-Fernandez <alomafernandezj@ufl.edu>
2025-03-24 19:12:30 -0400

    Created sign up page UI layout, missing backend functionality

--- moodmoney/src/main.jsx
+++ moodmoney/src/main.jsx
@@ -1,10 +1,17 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import App from './App.jsx'
+import Signup from './pages/Signup.jsx' // You’ll create this file if not already
+import './index.css'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/signup" element={<Signup />} />
+      </Routes>
+    </BrowserRouter>
+  </React.StrictMode>
 )
