774065cf95caab7960dba2dd2fe0490ccd763174
Joel Aloma-Fernandez <alomafernandezj@ufl.edu>
2025-03-24 20:11:07 -0400

    Added separate Welcome.jsk file for welcome page, and simplified App.jsx file

--- moodmoney/src/main.jsx
+++ moodmoney/src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import App from './App.jsx'
+import Welcome from './pages/Welcome'
 import Signup from './pages/Signup.jsx' 
 import Login from './pages/Login.jsx'
 import './index.css'
@@ -10,7 +10,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<App />} />
+        <Route path="/" element={<Welcome />} />
         <Route path="/signup" element={<Signup />} />
         <Route path="/login" element={<Login />} />
       </Routes>
