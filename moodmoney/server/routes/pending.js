1677cae422dbf5255f8f88b967081423fa1db800
William Opyrchal <william.opyrchal@gmail.com>
2025-03-29 13:00:15 -0400

    Fixed Date Logic

--- moodmoney/server/routes/pending.js
+++ moodmoney/server/routes/pending.js
@@ -14,20 +14,30 @@ router.get('/:userId', async (req, res) => {
 
     const now = new Date();
 
-    // Filter expenses made in the last 24 hours
-    const last24Hours = user.expenses.filter((expense) => {
+    
+
+// Filter expenses made in the last 24 hours (0-24 hours old)
+const last24Hours = user.expenses.filter((expense) => {
     const expenseDate = new Date(expense.date);
     const diffHours = (now - expenseDate) / (1000 * 60 * 60);
-    return diffHours <= 47;
-    });
+    return diffHours <= 24; // Only include expenses that are 0-24 hours old
+});
 
-    // Filter expenses made in the last week 
-    const lastWeek = user.expenses.filter((expense) => {
+// Filter expenses made between 24 hours and 1 week old (24 hours - 7 days old)
+const lastWeek = user.expenses.filter((expense) => {
     const expenseDate = new Date(expense.date);
     const diffHours = (now - expenseDate) / (1000 * 60 * 60);
     const diffDays = diffHours / 24;
-    return diffHours > 47 && diffDays <= 7;
-    });
+    return diffHours > 24 && diffDays <= 7; // Include expenses that are between 24 hours and 7 days old
+});
+
+// Filter expenses older than 1 week (more than 7 days old)
+const olderThanWeek = user.expenses.filter((expense) => {
+    const expenseDate = new Date(expense.date);
+    const diffDays = (now - expenseDate) / (1000 * 60 * 60 * 24);
+    return diffDays > 7; // Include expenses that are older than 7 days
+});
+
 
     return res.json({
       last24Hours,
